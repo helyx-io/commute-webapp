491fe28981272ea8b0888d2d3f5e935589e36fa7
Alexis Kinsella <alexis.kinsella@gmail.com>
2014-11-27 00:36:14 +0100

    Improved support for pems

--- src/javascript/models/pemClient.js
+++ src/javascript/models/pemClient.js
@@ -5,7 +5,8 @@ module.exports = function(sequelize, DataTypes) {
 	var PemClient = sequelize.define("PemClient", {
 		userId: DataTypes.INTEGER,
 		privateKey: DataTypes.STRING(2048),
-		certificate: DataTypes.STRING(2048)
+		certificate: DataTypes.STRING(2048),
+		fingerprint: DataTypes.STRING(128)
 	}, {
 		tableName: "pemClients"
 	});
--- src/javascript/routes/pemClients.js
+++ src/javascript/routes/pemClients.js
@@ -1,10 +1,9 @@
-
 ////////////////////////////////////////////////////////////////////////////////////
 // Imports
 ////////////////////////////////////////////////////////////////////////////////////
 
 var express = require('express');
-var pem = require('pem')
+var nodePem = require('pem');
 var models = require('../models');
 var security = require('../lib/security');
 
@@ -16,39 +15,98 @@ var security = require('../lib/security');
 var router = express.Router();
 
 router.get('/', security.ensureAuthenticated, (req, res) => {
-  models.PemClient.find({where: { userId: req.user.id }}).complete((err, pems) => {
-    res.render('pem-clients', { title: "Pem List", err: err, pems: pems });
-  });
+	models.PemClient.findAll({where: {userId: req.user.id}}).complete((err, pems) => {
+		res.render('pem-clients', {title: "Pem List", err: err, pems: pems});
+	});
+});
+
+router.get('/:id/remove', security.ensureAuthenticated, (req, res) => {
+	models.PemClient.find({where: {userId: req.user.id, id: req.params.id}}).complete((err, pem) => {
+		if (err != undefined) {
+			res.send(500, err.message);
+		}
+		else {
+			pem.destroy().complete((err, pem) => {
+				if (err != undefined) {
+					res.send(500, err.message);
+				}
+				else {
+					res.redirect("/pem-clients/");
+				}
+			});
+		}
+	});
 });
 
 router.get('/:id/private-key', security.ensureAuthenticated, (req, res) => {
-  models.PemClient.find({where: { userId: req.user.id, id: req.params.id }}).complete((err, pem) => {
-    res.contentType('text/plain');
-    res.send(pem.privateKey);
-  });
+	models.PemClient.find({where: {userId: req.user.id, id: req.params.id}}).complete((err, pem) => {
+		if (err != undefined) {
+			res.send(500, err.message);
+		}
+		else {
+			res.contentType('text/plain');
+			res.send(pem.privateKey);
+		}
+	});
 });
 
 router.get('/:id/certificate', security.ensureAuthenticated, (req, res) => {
-  models.PemClient.find({where: { userId: req.user.id, id: req.params.id }}).complete((err, pem) => {
-    res.contentType('text/plain');
-    res.send(pem.certificate);
-  });
+	models.PemClient.find({where: {userId: req.user.id, id: req.params.id}}).complete((err, pem) => {
+		if (err != undefined) {
+			res.send(500, err.message);
+		}
+		else {
+			res.contentType('text/plain');
+			res.send(pem.certificate);
+		}
+	});
+});
+
+router.get('/:id/certificate/infos', security.ensureAuthenticated, (req, res) => {
+	models.PemClient.find({where: {userId: req.user.id, id: req.params.id}}).complete((err, pem) => {
+		nodePem.readCertificateInfo(pem.certificate, (err, certInfos) => {
+			if (err != undefined) {
+				res.send(500, err.message);
+			}
+			else {
+				res.json(certInfos);
+			}
+		});
+	});
 });
 
 router.get('/generate', security.ensureAuthenticated, (req, res) => {
+	var options = {
+		days: 1,
+		keyBitsize: 2048,
+		hash: 'sha256',
+		emailAddress: req.user.email,
+		selfSigned: true
+	};
+	nodePem.createCertificate(options, (err, keys) => {
+		nodePem.getFingerprint(keys.certificate, (err, result) => {
 
-  pem.createCertificate({days:1, selfSigned:true}, (err, keys) => {
-
-    var pemClient = models.PemClient.build({
-      userId: req.user.id,
-      privateKey: keys.serviceKey,
-      certificate: keys.certificate
-    });
-    pemClient.save().complete((err, pemClient) => {
-      res.redirect("/pem-clients/");
-    });
-
-  });
+			if (err != undefined) {
+				res.send(500, err.message);
+			}
+			else {
+				var pemClient = models.PemClient.build({
+					userId: req.user.id,
+					privateKey: keys.serviceKey,
+					certificate: keys.certificate,
+					fingerprint: result.fingerprint
+				});
+				pemClient.save().complete((err, pemClient) => {
+					if (err != undefined) {
+						res.send(500, err.message);
+					}
+					else {
+						res.redirect("/pem-clients/");
+					}
+				});
+			}
+		});
+	});
 
 });
 
