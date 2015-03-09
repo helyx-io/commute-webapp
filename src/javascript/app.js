35d2c551cc04e06943be448a3aaa5e7a8126713d
Alexis Kinsella <alexis.kinsella@gmail.com>
2015-03-09 18:38:33 +0100

    Remove of monitoring

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -6,10 +6,6 @@ var passport = require('passport');
 var config = require('./conf/config');
 var logger = require('./log/logger');
 
-require('nodetime').profile({
-	accountKey: '9d1d8a854a009c31d69759e2e174b174f47ed537',
-	appName: 'Commute'
-});
 
 ////////////////////////////////////////////////////////////////////////////////////
 // Application Initialization
